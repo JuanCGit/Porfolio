27b87a4df5ed92a2dad373d5c08c5dfc034005b9
JuanCGit <juancasanovagago@gmail.com>
2023-05-11 16:44:44 +0200

    feat(header): finished header manager

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HeaderManagerService} from "../../services/header-manager.service";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  backgroundColor = 'white';
+  constructor(private headerManager: HeaderManagerService) {
+  }
 
+  ngOnInit() {
+    this.headerManager.headerColor$.subscribe((color) => this.backgroundColor = color)
+  }
 }
